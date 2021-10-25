1ea7aa8a7a01c2ae6fff5e6bb6042c53e831bda0
yj <yjha75@gmail.com>
2021-10-25 21:32:08 +0900

    강의듣고 코드수정

--- coordinates/main.js
+++ coordinates/main.js
@@ -1,5 +1,6 @@
-const result = document.getElementById("result");
-const cursor = document.getElementById("cursor");
+const vertical = document.querySelector(".vt");
+const horizontal = document.querySelector(".hl");
+const cursor = document.getElementById("tag");
 const target = document.querySelector(".target");
 
 function crossLine(e) {
@@ -13,5 +14,16 @@ function resultTwo(e) {
   cursor.innerText = `${e.clientX} ${e.clientY}`
 }
 
-document.addEventListener("mousemove", crossLine);
-document.addEventListener("mousemove", resultTwo);
+document.addEventListener("mousemove", event => {
+  const x = event.clientX;
+  const y = event.clientY;
+  
+  vertical.style.left =`${x}px`;
+  horizontal.style.top =`${y}px`;
+  target.style.left = `${x}px`;
+  target.style.top =`${y}px`;
+  tag.style.left = `${x}px`;
+  tag.style.top =`${y}px`;
+  tag.innerHTML = `${x}px ${y}px`
+});
+ 
