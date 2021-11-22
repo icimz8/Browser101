21434718b30da4472e9d648c7bd83bcb290bb700
yj <yjha75@gmail.com>
2021-11-23 01:34:10 +0900

    타이머 중지기능 수정

--- carrot/main.js
+++ carrot/main.js
@@ -1,21 +1,26 @@
 const start = document.querySelector('.start-button');
+const replay = document.querySelector('.replay');
 const gamefield = document.querySelector('.game-field');
 const fieldRect = gamefield.getBoundingClientRect();
 const items = document.querySelector('.items');
-
+const timer = document.querySelector('.timer');
+let downloadTimer;
+let timeleft = 8;
 
 function gameset() {
   //벌레와 당근을 생성하고 field에 추가
-  //  console.log(fieldRect);
   createItem('carrot', 10, 'img/carrot.png');
   createItem('bug', 8, 'img/bug.png');
   updateTimer();
-
+   downloadTimer = setInterval(updateTimer, 1000);
 };
 
-function createItem(className, count, imgPath, padding) {
+function resetGame(){
+  gameset();
+}
+function createItem(className, count, imgPath) {
   let id = 0;
-  for (i = 0; i <= count; i++) {
+  for (i = 0; i < count; i++) {
     const item = document.createElement('img');
     item.setAttribute('class', className);
     item.setAttribute('src', imgPath);
@@ -24,53 +29,66 @@ function createItem(className, count, imgPath, padding) {
     item.getBoundingClientRect();
     const x1 = Math.floor(Math.random() * (fieldRect.height / 2 - 80));
     const y1 = Math.floor(Math.random() * (fieldRect.width - 80));
-
     item.style.position = 'absolute';
     item.style.top = 300 + x1 + "px";
     item.style.left = y1 + "px";
     items.appendChild(item);
-    item.addEventListener('click',  harvestCarrot);
-  }
 
+    item.addEventListener('click', harvestCarrot);
+  }
 };
 
 function updateTimer() {
-  const timer = document.querySelector('.timer');
-  let timeleft = 20;
-  const downloadTimer = setInterval(function () {
-    timeleft--;
-    timer.textContent = `0:${timeleft}`;
-    if (timeleft <= 0) {
-      clearInterval(downloadTimer);
-      gameOver();
-    }
-  }, 1000)
+  if (timeleft === 0) {
+    clearInterval(downloadTimer);
+    gameOver(0);
+    return;
+  }
+  timeleft--;
+  timer.textContent = `0:${timeleft}`;
+  return;
 }
-
-function gameOver() {
+ 
+function gameOver(end) {
   const gameover = document.querySelector('.game-over');
   const result = gameover.querySelector('span');
-  result.innerHTML = `YOU LOSE`;
-  gameover.classList.remove('hidden');
-  // if()
+  if (end === 0) {
+    result.innerHTML = `YOU LOSE`;
+    gameover.classList.remove('hidden');
+  } else if (end === 1) {
+    result.innerHTML = `YOU WON`;
+    gameover.classList.remove('hidden');
+  };
 }
 
 function harvestCarrot(event) {
-    const id = event.target.dataset.id;
-    const rest = document.querySelector('.the-rest');
-    let carrotleft = items.childElementCount;
-    console.log(carrotleft);
+  const id = event.target.dataset.id;
+  const rest = document.querySelector('.the-rest');
+  let carrotleft = items.getElementsByClassName('carrot').length;
 
-    if (id && event.target.tagName ==="IMG" ) {
-      const toBeDeleted = document.querySelector(`.carrot[data-id ="${id}"]`)
-      toBeDeleted.remove();
-      carrotleft--;
-    }
-    rest.innerHTML =`${carrotleft}`
+
+  if (id && event.target.className === "carrot") {
+    const toBeDeleted = document.querySelector(`.carrot[data-id ="${id}"]`)
+    toBeDeleted.remove();
+    carrotleft--;
+    rest.innerHTML = carrotleft;
+  } else if (event.target.className === "bug") {
+    gameOver(0);
+    clearInterval(downloadTimer);
   }
+  if (carrotleft == 0) {
+    gameOver(1);
+    clearInterval(downloadTimer);
+  }
+}
 
 
 
-window.addEventListener('load', ( ) => {
-  start.addEventListener('click', gameset ,{once : true} );
+window.addEventListener('load', () => {
+  start.addEventListener('click', gameset, {
+    once: true
+  });
+  replay.addEventListener('click', resetGame, {
+    once: true
+  });
 });
